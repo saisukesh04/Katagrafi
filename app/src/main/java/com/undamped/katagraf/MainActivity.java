56312a12e6d6ce3a0301ee4c069e4cdb17686e54
Sai Sukesh <sukesh.sai04@gmail.com>
2021-04-12 09:54:43 +0530

    Added code for login and register

--- app/src/main/java/com/undamped/katagraf/MainActivity.java
+++ app/src/main/java/com/undamped/katagraf/MainActivity.java
@@ -2,13 +2,35 @@ package com.undamped.katagraf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (mAuth.getCurrentUser() == null) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
     }
 }
--- app/src/main/java/com/undamped/katagraf/RegisterActivity.java
+++ app/src/main/java/com/undamped/katagraf/RegisterActivity.java
@@ -2,13 +2,78 @@ package com.undamped.katagraf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    @BindView(R.id.userName) EditText userName;
+    @BindView(R.id.emailREditText) EditText emailREditText;
+    @BindView(R.id.passwordREditText) EditText passwordREditText;
+    @BindView(R.id.confirmPassEditText) EditText confirmPassEditText;
+    @BindView(R.id.registerBtn) Button registerBtn;
+    @BindView(R.id.clickLogin) TextView clickLogin;
+    @BindView(R.id.regProgressBar) ProgressBar regProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        ButterKnife.bind(this);
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+        clickLogin.setOnClickListener(view -> {
+            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            finish();
+        });
+
+        registerBtn.setOnClickListener(v -> {
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            View focusedView = RegisterActivity.this.getCurrentFocus();
+            if (focusedView != null) {
+                imm.hideSoftInputFromWindow(focusedView.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+            }
+
+            String email = emailREditText.getText().toString();
+            String password = passwordREditText.getText().toString();
+            String confPassword = confirmPassEditText.getText().toString();
+
+            if (email.isEmpty() || password.isEmpty() || confPassword.isEmpty())
+                Snackbar.make(v, "Please fill all the fields", Snackbar.LENGTH_LONG).show();
+            else if (password.length() < 8)
+                Snackbar.make(v, "Password should contain at least 8 characters", Snackbar.LENGTH_LONG).show();
+            else if (!confPassword.equals(password))
+                Snackbar.make(v, "Passwords don't match", Snackbar.LENGTH_LONG).show();
+            else {
+                regProgressBar.setVisibility(View.VISIBLE);
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(RegisterActivity.this, task -> {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(RegisterActivity.this, "Registration successful. Logging in", Toast.LENGTH_LONG).show();
+                                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                                //TODO : storeUserDataToDb();
+                            } else {
+                                regProgressBar.setVisibility(View.INVISIBLE);
+                                Toast.makeText(RegisterActivity.this, "Registration failed. Try again", Toast.LENGTH_LONG).show();
+                                Log.e("Error: Register", "Error while adding data to database\n" + task.getException());
+                            }
+                        });
+            }
+        });
     }
 }
