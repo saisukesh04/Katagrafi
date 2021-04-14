47a7085b3815e1114e16b0c2d03e7240fc5bbe0d
Sai Sukesh <sukesh.sai04@gmail.com>
2021-04-14 09:21:42 +0530

    Added scanner

--- app/src/main/java/com/undamped/katagraf/fragments/ScanFragment.java
+++ app/src/main/java/com/undamped/katagraf/fragments/ScanFragment.java
@@ -1,18 +1,29 @@
 package com.undamped.katagraf.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.undamped.katagraf.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class ScanFragment extends Fragment {
+
+    @BindView(R.id.scan_barcode) ImageView scan_barcode;
+    @BindView(R.id.productID) EditText productID;
 
     public ScanFragment() {
     }
@@ -23,6 +34,31 @@ public class ScanFragment extends Fragment {
 
         ButterKnife.bind(this, root);
 
+        scan_barcode.setOnClickListener(view -> {
+            IntentIntegrator intentIntegrator = new IntentIntegrator(this.getActivity()).forSupportFragment(this);
+            intentIntegrator.setPrompt("Scan a item's QR Code");
+            intentIntegrator.setOrientationLocked(false);
+            intentIntegrator.initiateScan();
+        });
+
         return root;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        // if the intentResult is null then toast a message as "cancelled"
+        if (intentResult != null) {
+            if (intentResult.getContents() == null) {
+                Toast.makeText(getContext(), "Scan cancelled", Toast.LENGTH_SHORT).show();
+            } else {
+                // if the intentResult is not null we'll set the content and format of scan message
+                productID.setText(intentResult.getContents());
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
